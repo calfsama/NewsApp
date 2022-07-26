1fb83e3879a33c39c4fb7342aaa3dc43e3e2ba2c
calfsama <tomirisnegmatova@gmail.com>
2022-07-26 10:41:43 +0500

    Initial Commit

---
+++ NewsApp/Models/Models.swift
@@ -0,0 +1,15 @@
+//
+//  Models.swift
+//  CustomCells
+//
+//  Created by Tomiris Negmatova on 25/07/22.
+//
+
+import Foundation
+
+
+struct Models {
+    var imageName: String
+    var title: String
+    var source: String
+}
---
+++ NewsApp/Views/TableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewCell.swift
+//  CustomCells
+//
+//  Created by Tomiris Negmatova on 25/07/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    static let identifier = "TableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "TableViewCell", bundle: nil)
+    }
+    
+//    public func configure(with title: String, imageName: String) {
+//        myLabel.text = title
+//        myImage.image = UIImage(systemName: imageName)
+//    }
+    
+    
+    
+    @IBOutlet var myImage: UIImageView!
+    @IBOutlet var myLabel: UILabel!
+    @IBOutlet var sources: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
